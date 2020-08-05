179c6fc673dfef55b61420ca3228cffee71c6066
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-05 01:29:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,10 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
-  def initialize(sender, receiver, amount)
+  def initialize(sender, receiver, amount=50)
     @sender=sender
     @receiver=receiver
     @status = "pending"
-    @amount=50
   end
 
   def valid?
